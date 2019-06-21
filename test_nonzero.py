e68e88a7f5764c1b30731942d9950f253ad004db
majvazov <32581471+majvazov@users.noreply.github.com>
2019-06-21 15:16:47 +0300

    Delete test_nonzero.py

--- test_nonzero.py
+++
@@ -1,9 +0,0 @@
-class Person:
-    def __init__(self, weight, hight):
-        self.weight = weight
-        self.hight = hight
-
-    def __nonzero__(self):
-        return bool(self.weight or self.hight)
-
-p = Person(23,23)
