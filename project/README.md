974ad67429d19935a0840e5d75da98275ebeb147
Martin Ayvazov <majvazov@tu-sofia.bg>
2019-06-24 03:23:59 +0300

    Added images and better output

--- project/README.md
+++ project/README.md
@@ -4,10 +4,5 @@ Pair of employees that has worked together for the longest time period. It was d
 Problems That I Solved:
 1. Finding the pair of employees that have longest time together
 2. Simulating data algorithm
-3. Except holidays for starting a project
-4. Building custom exceptions
-5. Validation of input data.
-6. Detecting range overlap
-7. Reading and Writing files in python
-
-![Image of Yaktocat](https://raw.githubusercontent.com/majvazov/martin-ayvazov-employees/master/asd.png)
+3. Detecting range overlap
+4. Detecting multiple range overlap
--- project/longest_pair.py
+++ project/longest_pair.py
@@ -10,7 +10,14 @@ if  __name__ == "__main__":
 
     try:
         RANGES = find_all_ranges(open(os.path.join(sys.path[0], "test.txt"), "r"))
-        print(find_longest_time_couple(RANGES))
+        LONGEST_TIME_PAIR = find_longest_time_couple(RANGES)
+
+        #output
+        print("projects: {}".format(LONGEST_TIME_PAIR[0]))
+        print("first employee: {}".format(LONGEST_TIME_PAIR[1]))
+        print("second employee: {}".format(LONGEST_TIME_PAIR[2]))
+        print("ranges in days: {}".format(LONGEST_TIME_PAIR[3]))
+
 
     except IOError:
         print("Cant open file test.txt")
